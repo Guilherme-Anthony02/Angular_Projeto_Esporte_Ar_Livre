b91ddcdfc0f6b8aed27886d220db518a8d047bb8
Guilherme <guilherme.anthonysantos02@gmail.com>
2026-08-12 21:42:44 -0300

    configurações realizadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { MenuComponent } from './component/menu-component/menu-component';
+import { HomeComponent } from './component/home-component/home-component';
+import { AtletaComponent } from './component/atleta-component/atleta-component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo:"/home",
+        pathMatch: 'full'
+    },
+
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: "cadastroatleta",
+        component:AtletaComponent
+    }
+
+];
