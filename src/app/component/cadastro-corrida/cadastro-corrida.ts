2e12e90324a85822245cfb43417a50ac868092b0
Guilherme <guilherme.anthonysantos02@gmail.com>
2026-08-19 21:14:41 -0300

    alterações realizadas em cadastro-corrida

--- src/app/component/cadastro-corrida/cadastro-corrida.ts
+++ src/app/component/cadastro-corrida/cadastro-corrida.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Corrida } from '../../models/corrida';
+import { CorridaService } from '../../service/corrida-service';
 
 @Component({
   selector: 'app-cadastro-corrida',
@@ -8,10 +10,33 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './cadastro-corrida.css',
 })
 export class CadastroCorrida {
+  id = 0
   descricaoCorrida: string = ''
-  data: Date = new Date
-  distancia: string = ''
+  dataCorrida: string = ''
+  distancia5km = false
+  distancia10km = false
+  distancia25km = false
+
+  constructor(private corridaService: CorridaService) {}
+
+  dadosFormulario(){
+    const corrida = new Corrida()
+    corrida.descricaoCorrida = this.descricaoCorrida
+    corrida.dataCorrida = this.dataCorrida
+    corrida.distancia5km = this.distancia5km
+    corrida.distancia10km = this.distancia10km
+    corrida.distancia25km = this.distancia25km
+
+    this.corridaService.salvarCorrida(corrida)
+  }
   
+  limparAtributos(){
+    this.descricaoCorrida = ''
+    this.dataCorrida = ''
+    this.distancia5km = false
+    this.distancia10km = false
+    this.distancia25km = false
+  }
 
 
 }
