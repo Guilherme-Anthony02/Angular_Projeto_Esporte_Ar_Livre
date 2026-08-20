babcc1f1b91364691036bbee4ab1b042fe0cca23
Guilherme <guilherme.anthonysantos02@gmail.com>
2026-08-20 20:58:32 -0300

    rotas pra lista de corridas adicionada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './component/home-component/home-component';
 import { AtletaComponent } from './component/atleta-component/atleta-component';
 import { CadastroCorrida } from './component/cadastro-corrida/cadastro-corrida';
 import { AtletaListaComponent } from './component/atleta-lista-component/atleta-lista-component';
+import { CorridaListaComponent } from './corrida-lista-component/corrida-lista-component';
 
 
 export const routes: Routes = [
@@ -34,6 +35,16 @@ export const routes: Routes = [
     {
         path: "Cadastro_Corrida",
         component: CadastroCorrida
-    }
+    },
+
+    {
+        path: "alteracorrida/:id",
+        component: CadastroCorrida
+    },
+
+    {
+        path: "listacorrida",
+        component: CorridaListaComponent
+    },
 
 ];
