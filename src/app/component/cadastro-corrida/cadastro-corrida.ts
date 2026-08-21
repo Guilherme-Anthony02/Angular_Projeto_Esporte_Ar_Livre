c57b37cfdd0b55ee6d693e8f1e0a74a5e1e18c15
Guilherme <guilherme.anthonysantos02@gmail.com>
2026-08-20 21:02:06 -0300

    pequenas correções realizadas

--- src/app/component/cadastro-corrida/cadastro-corrida.ts
+++ src/app/component/cadastro-corrida/cadastro-corrida.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Corrida } from '../../models/Corrida';
 import { CorridaService } from '../../service/corrida-service';
+import { ActivatedRoute } from '@angular/router';
+
 
 
 @Component({
@@ -18,9 +20,28 @@ export class CadastroCorrida {
   distancia10km = false
   distancia25km = false
 
-  constructor(private corridaService: CorridaService) {}
+  idCorrida = 0
+  editar = false
 
-  dadosFormulario(){
+  constructor(
+    private corridaService: CorridaService,
+    private activeRoute: ActivatedRoute,
+    private cdr: ChangeDetectorRef
+
+  ) { }
+
+  ngOnInit() {
+    this.idCorrida = Number(this.activeRoute.snapshot.paramMap.get('id'))
+
+    if (this.idCorrida > 0) {
+      this.editar = true
+      this.carregaDados(this.idCorrida)
+    }
+
+  }
+
+  //FUNÇÃO PARA CADASTRAR E ALTERAR
+  dadosFormulario() {
     const corrida = new Corrida()
     corrida.descricaoCorrida = this.descricaoCorrida
     corrida.dataCorrida = this.dataCorrida
@@ -28,10 +49,54 @@ export class CadastroCorrida {
     corrida.distancia10km = this.distancia10km
     corrida.distancia25km = this.distancia25km
 
-    this.corridaService.salvarCorrida(corrida)
+    if (this.editar) {
+      corrida.id = this.idCorrida
+      
+      this.corridaService.alterarCorrida(corrida)
+        .subscribe({
+          next: (respostaAPI) => {
+            return respostaAPI
+          },
+          error: (msgErro) => {
+            return msgErro
+          }
+        })
+
+    } else {
+      this.corridaService.salvarCorrida(corrida)
+        .subscribe({
+          next: (respostaAPI) => {
+            return respostaAPI
+          },
+          error: (msgErro) => {
+            return msgErro
+          }
+        })
+    }
+
+    this.limparAtributos()
+
   }
-  
-  limparAtributos(){
+
+  carregaDados(idCorrida: number) {
+    this.corridaService.listarCorrida(idCorrida)
+      .subscribe({
+        next: (dadosCorrida) => {
+          this.descricaoCorrida = dadosCorrida.descricaoCorrida
+          this.dataCorrida = dadosCorrida.dataCorrida
+          this.distancia5km = dadosCorrida.distancia5km
+          this.distancia10km = dadosCorrida.distancia10km
+          this.distancia25km = dadosCorrida.distancia25km
+
+          this.cdr.detectChanges()
+        },
+        error: (msgErro) => {
+          return msgErro
+        }
+      })
+  }
+
+  limparAtributos() {
     this.descricaoCorrida = ''
     this.dataCorrida = ''
     this.distancia5km = false
