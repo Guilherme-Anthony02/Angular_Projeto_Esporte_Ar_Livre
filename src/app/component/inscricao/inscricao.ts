f589014f2d21b1eeb0c49bd5ea609fb0015b59eb
Guilherme <guilherme.anthonysantos02@gmail.com>
2026-08-21 17:01:50 -0300

    logica pra puxar os dados da api de corrida e atleta para inscricao

--- src/app/component/inscricao/inscricao.ts
+++ src/app/component/inscricao/inscricao.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Atleta } from '../../models/Atleta';
+import { Corrida } from '../../models/Corrida';
+import { AtletaService } from '../../service/atleta-service';
+import { CorridaService } from '../../service/corrida-service';
 
 @Component({
   selector: 'app-inscricao',
@@ -9,13 +13,32 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./inscricao.css'],
 })
 export class Inscricao {
-  atleta = '';
-  cpf = '';
+  atletas: Atleta[] = [];
+  corridas: Corrida[] = [];
+  atletaId: number | null = null;
+  corridaId: number | null = null;
   distancia5km = '';
   distancia10km = '';
   distancia25km = '';
   camisetaTamanho = '';
   faixaEtaria = '';
+
+  constructor(
+    private atletaService: AtletaService,
+    private corridaService: CorridaService,
+  ) { }
+
+  ngOnInit() {
+    this.atletaService.listarAtletas().subscribe({
+      next: (atletas) => this.atletas = atletas,
+      error: (erro) => console.error('Erro ao listar atletas para inscrição', erro),
+    });
+
+    this.corridaService.listarCorridas().subscribe({
+      next: (corridas) => this.corridas = corridas,
+      error: (erro) => console.error('Erro ao listar corridas para inscrição', erro),
+    });
+  }
 
   enviaDadosInscricao() {
     console.log('Inscrição enviada');
